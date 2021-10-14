534a93189c1ac9548f8acfda7f256662208e7f0a
Marcus J Hatton <mjh1n20@soton.ac.uk>
2021-10-14 17:48:03 +0100

    full jac solver

--- JacobianSolverOnly.py
+++ JacobianSolverOnly.py
@@ -17,23 +17,34 @@ dtcons = [D.diff(t), S1.diff(t), S2.diff(t), S3.diff(t), E.diff(t)]
 # Prims
 v1, v2, v3 = sp.Function('v1')(t, x, y, z), sp.Function('v2')(t, x, y, z), sp.Function('v3')(t, x, y, z)
 vs = [v1, v2, v3]
+W = sp.Function('W')(t, x, y, z)
+
 n = sp.Function('n')(t, x, y, z)
 rho = sp.Function('rho')(t, x, y, z)
-#p = sp.Function('p')(t, x, y, z)
-p = sp.Function('p')(rho, n)
+p = sp.Function('p')(t, x, y, z)
+#p = sp.Function('p')(rho, n)
+Gamma = sp.symbols('Gamma')
 prim_vars = [v1, v2, v3, p, n, rho]
 
-# state vector
-D = n
-S1 = (rho + p)*v1
-S2 = (rho + p)*v2
-S3 = (rho + p)*v3
-E = rho
+# state vector simplest
+# D = n
+# S1 = (rho + p)*v1
+# S2 = (rho + p)*v2
+# S3 = (rho + p)*v3
+# E = rho
+
+# better
+D = (rho + p/(1-Gamma))*W # n is substituted here
+S1 = (rho + p)*v1*W**2
+S2 = (rho + p)*v2*W**2
+S3 = (rho + p)*v3*W**2
+E = (rho + p)*W**2 - p
+
 sv = [D, S1, S2, S3, E]
 
 # Declare the vars that we need to calculate the Jacobian of the state vector wrt
 # (Essentially the variables that have time derivatives in the CE expansion)
-jac_vars = [n, rho, v1, v2, v3]
+jac_vars = [p, rho, v1, v2, v3]
 #jac_vars = [W, v1, v2, v3]
 
 # Convert the state and flux vectors into Matrices so that the Jacobian
@@ -46,7 +57,15 @@ sv_Mat = sp.Matrix(sv)
 sv_Jac = sv_Mat.jacobian(jac_vars_Mat)
 sv_Jac_inv = sv_Jac.inv()
 
+#jac_sol_simple_outfile = open('jac_sol_simple.txt','w')
+jac_sol_full_outfile = open('jac_sol_full.txt','w')
+
 for i in range(len(jac_vars)):
     print(str(jac_vars[i].diff(t))+': \n')
     print((sv_Jac_inv*sp.Matrix(dtcons))[i])
     print('\n')
+    jac_sol_full_outfile.write(str((sv_Jac_inv*sp.Matrix(dtcons))[i])+'\n')
+
+#jac_sol_simple_outfile.close()
+jac_sol_full_outfile.close()
+
